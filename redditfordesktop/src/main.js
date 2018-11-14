d5c6fc903ae4471b6f9b9fc87149ba150a2c2b67
Hemant Kunda <hkunda@usc.edu>
2018-11-13 19:33:40 -0800

    create-react-app bootstrapped with electron

---
+++ redditfordesktop/src/main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    console.log('hello');
+    mainWindow.loadURL('http://localhost:3000');
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});

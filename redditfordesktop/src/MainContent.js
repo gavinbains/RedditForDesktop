df40b85ed2e89d9c172d567d4be8e7d83380276d
Hemant Kunda <hkunda@usc.edu>
2018-11-27 12:38:32 -0800

    upvote/downvote ui

--- redditfordesktop/src/MainContent.js
+++ redditfordesktop/src/MainContent.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import marked from 'marked';
 
+import dvActive from './img/dv_active.png';
+import dvInactive from './img/dv_inactive.png';
+import uvActive from './img/uv_active.png';
+import uvInactive from './img/uv_inactive.png';
+
 export default class MainContent extends Component {
     constructor(props) {
         super(props);
@@ -25,7 +30,7 @@ export default class MainContent extends Component {
         const {threads} = this.props;
         return (
             <div className="main-content-container">
-                <div className="main-content-thread-preview">
+                <div className="main-content-thread-preview-container">
                 {
                     threads.map((thread, idx) => <ThreadPreview key={idx} index={idx} threadInfo={thread} selected={idx === threadIdx} onclick={this._clickThread}/>)
                 }
@@ -41,10 +46,50 @@ export default class MainContent extends Component {
     }
 }
 
-const ThreadPreview = ({index, threadInfo, selected, onclick}) => {
-    return (
-        <h2 className={selected ? 'preview-selected' : 'preview-not-selected'} onClick={() => onclick(index)}>
-            <p>{threadInfo.data.title}</p>
-        </h2>
+class ThreadPreview extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userScore: 0
+        };
+    }
+
+    componentDidMount() {
+        document.addEventListener('keypress', e => {
+            if (!this.props.selected) return;
+            if (e.keyCode === 113) this._vote(1);
+            if (e.keyCode === 101) this._vote(-1);
+        })
+    }
+
+    _vote = (v) => {
+        this.setState(prev => {
+            let {userScore} = prev;
+            if (userScore === v) userScore = 0;
+            else userScore = v;
+            return {
+                userScore
+            };
+        });
+    }
+
+    render() {
+        const {index, threadInfo, selected, onclick} = this.props;
+        const {userScore} = this.state;
+        const selectedClass = selected ? 'preview-selected' : 'preview-not-selected';
+        const uv = userScore === 1 ? uvActive : uvInactive;
+        const dv = userScore === -1 ? dvActive : dvInactive;
+        return (
+        <div className={`preview-container ${selectedClass}`}>
+            <div className="preview-upvotes">
+            <img className="preview-vote" src={uv} onClick={() => this._vote(1)}></img>
+            <p>{threadInfo.data.score + userScore}</p>
+            <img className="preview-vote" src={dv} onClick={() => this._vote(-1)}></img>
+            </div>
+            <h2 className="preview-content" onClick={() => onclick(index)}>
+                <p>{threadInfo.data.title}</p>
+            </h2>
+        </div>
     );
+    }
 }
